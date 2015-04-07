8dfd3660df92433a9d5cab423fb038f2aeed290f
danimtb <danimanzaneque@gmail.com>
2015-04-07 17:32:57 +0200

    main in teleop_pioneer updated

--- teleop_pioneer/src/main.cpp
+++ teleop_pioneer/src/main.cpp
@@ -3,13 +3,17 @@
 #include <signal.h>
 #include <termios.h>
 #include <stdio.h>
-#include <iostream>
 
 #include "rosaria/BumperState.h"
 
+#define KEYCODE_R 0x43
+#define KEYCODE_L 0x44
+#define KEYCODE_U 0x41
+#define KEYCODE_D 0x42
+#define KEYCODE_Q 0x71 ////////DEFINIR BARRA ESPACIADORA
+
 using geometry_msgs::Twist;
 using namespace std;
-
 
 class TeleopPioneer
 {
@@ -17,33 +21,82 @@ public:
 	TeleopPioneer();
 	void keyLoop();
 	
+	ros::NodeHandle n; 
+		
+	ros::Publisher chatter_pub;
 	ros::Time t1;
-	
-private:
-	ros::NodeHandle n;	
-	ros::Publisher publicadorVelocidad;
 	Twist vel;
+	//int kfd = 0;
+	//struct termios cooked, raw;
 	unsigned int temp=0;
 	
 };
 
 TeleopPioneer::TeleopPioneer()
 {
-	publicadorVelocidad = n.advertise<geometry_msgs::Twist>("RosAria/cmd_vel", 1);
+	//nh_.param("scale_angular", a_scale_, a_scale_);
+	//nh_.param("scale_linear", l_scale_, l_scale_);
+	//twist_pub_ = nh_.advertise<geometry_msgs::Twist>("turtle1/cmd_vel", 1);
+	chatter_pub = n.advertise<geometry_msgs::Twist>("RosAria/cmd_vel", 1);
+}
+
+int kfd = 0;
+struct termios cooked, raw;
+
+void quit(int sig)
+{
+	tcsetattr(kfd, TCSANOW, &cooked);
+	ros::shutdown();
+	exit(0);
+}
+
+int main(int argc, char** argv)
+{
+	ros::init(argc, argv, "teleop_turtle");
+	TeleopPioneer teleop_pioneer;
+	signal(SIGINT,quit);
+	
+	ros::init(argc, argv, "main"); 
+	signal(SIGINT,quit);
+	ros::Rate r(5);
+	teleop_pioneer.t1=ros::Time::now();	
+	
+	teleop_pioneer.keyLoop();
+	return(0);
 }
 
 void TeleopPioneer::keyLoop()
 {
 	char c;
 	bool dirty=false;
-	
+	// get the console in raw mode
+	tcgetattr(kfd, &cooked);
+	memcpy(&raw, &cooked, sizeof(struct termios));
+	raw.c_lflag &=~ (ICANON | ECHO);
+	// Setting a new line, then end of file
+	raw.c_cc[VEOL] = 1;
+	raw.c_cc[VEOF] = 2;
+	tcsetattr(kfd, TCSANOW, &raw);
+	puts("Reading from keyboard");
+	puts("---------------------------");
+	puts("Use arrow keys to move Pioneer");
+
+
 	while(ros::ok)
 	{
-		cin>>c;
+		// get the next event from the keyboard
+
+		if(read(kfd, &c, 1) < 0)
+		{
+			perror("read():");
+			exit(-1);
+		}
+
+		ROS_DEBUG("value: 0x%02X\n", c);
 
 		switch(c)
 		{
-			case 'a':
+			case KEYCODE_L:
 				ROS_DEBUG("LEFT");
 				vel.linear.x = 0;
 				vel.linear.y=0;
@@ -55,7 +108,7 @@ void TeleopPioneer::keyLoop()
 				dirty = true;
 				break;
 
-			case 'd':
+			case KEYCODE_R:
 				ROS_DEBUG("RIGHT");
 				vel.linear.x = 0;
 				vel.linear.y=0;
@@ -67,9 +120,9 @@ void TeleopPioneer::keyLoop()
 				dirty = true;
 				break;
 
-			case 'w':
+			case KEYCODE_U:
 				ROS_DEBUG("UP");
-				vel.linear.x = 0.3;
+				vel.linear.x = 0.4;
 				vel.linear.y=0;
 				vel.linear.z=0;
 				vel.angular.x = 0;
@@ -79,9 +132,9 @@ void TeleopPioneer::keyLoop()
 				dirty = true;
 				break;
 
-			case 's':
+			case KEYCODE_D:
 				ROS_DEBUG("DOWN");
-				vel.linear.x = -0.3;
+				vel.linear.x = -0.4;
 				vel.linear.y=0;
 				vel.linear.z=0;
 				vel.angular.x = 0;
@@ -91,7 +144,7 @@ void TeleopPioneer::keyLoop()
 				dirty = true;
 				break;
 				
-			default: //STOP with 'q'
+			case KEYCODE_Q: //REEEEEVIIIIIISAAAAAAAARRRRRR
 				ROS_DEBUG("Q");
 				vel.linear.x = 0;
 				vel.linear.y=0;
@@ -104,7 +157,12 @@ void TeleopPioneer::keyLoop()
 				break;
 		}
 
-		publicadorVelocidad.publish(vel);
+
+		if(dirty ==true)
+		{
+			chatter_pub.publish(vel);
+			dirty=false;
+		}
 		
 		ros::Duration(0.1).sleep(); // sleep for one tenth of a second
 		ros::spinOnce();
@@ -112,25 +170,5 @@ void TeleopPioneer::keyLoop()
 	}
 	
 return;
-}
-
-
-
-void quit(int sig)
-{
-	ros::shutdown();
-	exit(0);
-}
-
-int main(int argc, char** argv)
-{
-	ros::init(argc, argv, "teleop_pioneer");
-	TeleopPioneer teleop_pioneer;
-	signal(SIGINT, quit);
-	ros::Rate r(5);
-	//teleop_pioneer.t1=ros::Time::now();	
-	
-	teleop_pioneer.keyLoop();
-	return(0);
 }
 	
