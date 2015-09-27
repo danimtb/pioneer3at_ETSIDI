79683a25ac2df576040a6d3e33612b52bc9d00e6
Daniel <danimanzaneque@gmail.com>
2015-09-27 19:52:21 +0200

    gazebo support, navigation tweaks, depthimage_to_laser in kinect (normal and low), rosaria launch robotmodel

---
+++ pioneer_utils/src/points.cpp
@@ -0,0 +1,25 @@
+#include "ros/ros.h"
+#include "beginner_tutorials/AddTwoInts.h"
+#include <cstdlib>
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "points");
+
+  ros::NodeHandle n;
+  ros::ServiceClient client = n.serviceClient<beginner_tutorials::AddTwoInts>("add_two_ints");
+  beginner_tutorials::AddTwoInts srv;
+  srv.request.a = atoll(argv[1]);
+  srv.request.b = atoll(argv[2]);
+  if (client.call(srv))
+  {
+    ROS_INFO("Sum: %ld", (long int)srv.response.sum);
+  }
+  else
+  {
+    ROS_ERROR("Failed to call service add_two_ints");
+    return 1;
+  }
+
+  return 0;
+}
